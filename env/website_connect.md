6c90afab7d6fcb6d687641e73caa832f5540f85b
Manfredi Carta <manfredi.carta@gmail.com>
2026-05-31 22:17:53 +0200

    RSVP edits, QR code generator

---
+++ env/website_connect.md
@@ -0,0 +1,11 @@
+```
+npm run build
+Remove-Item Env:WEBSITE_PW -ErrorAction SilentlyContinue
+npm run preview:protected
+```
+
+http://127.0.0.1:4321/
+
+
+
+https://gabyandmanfredi.net/admin/rsvp/
---
+++ src/qr/qr_code.R
@@ -0,0 +1,22 @@
+library(qrcode)
+
+qr <- qr_code("https://gabyandmanfredi.net", ecl = "M")
+
+
+# Save as SVG
+generate_svg(
+  qrcode = qr,
+  filename = "gabyandmanfredi_qr.svg",
+  size = 1000,
+  foreground = "#1e2d27",
+  background = "#faf6ef",
+  show = FALSE
+)
+
+qr <- qr_code("https://gabyandmanfredi.net", ecl = "H")
+
+# Preview in R
+plot(
+  qr,
+  col = c("#faf6ef", "#31483d")
+)
